baa39a130683c8932edb970b70cbbcb44d43d131
Bharanidharan Murugaiyan <m.bharanidharan88@gmail.com>
2023-09-02 09:36:52 +0100

    Clean Directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
 import addImage from "./add-image";
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
+const heading = new Heading();
+heading.render();
 // addImage();
