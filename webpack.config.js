dcaec868c653997c92be9105768dba5dd82028ca
Bharanidharan Murugaiyan <m.bharanidharan88@gmail.com>
2023-09-02 09:45:59 +0100

    HTML Webpack Plugin - Generating HTML file in dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -68,6 +69,9 @@ module.exports = {
         "**/*",
         path.join(process.cwd(), "build/**/*"),
       ],
+    }),
+    new HtmlWebpackPlugin({
+      publicPath: "",
     }),
   ],
 };
