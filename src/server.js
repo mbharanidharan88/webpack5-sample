429bed8623c2c8efac0fe56e726497a334c329d7
Bharanidharan Murugaiyan <m.bharanidharan88@gmail.com>
2023-09-04 16:54:16 +0100

    Express Integration

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Test Content");
+app.get("/hello-world", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+
+  res.send(contentFromHtmlFile);
 });
+
+app.get("/elephant", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/elephant.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+
+  res.send(contentFromHtmlFile);
+});
+
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(3000, function () {
   console.info("Application is running on http://localhost:3000/");
