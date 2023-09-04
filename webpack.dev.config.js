51a15cb2049dcd401f4fdbbb6a272900615cd2b1
Bharanidharan Murugaiyan <m.bharanidharan88@gmail.com>
2023-09-04 16:31:11 +0100

    Dev Config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,9 +4,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    elephant: "./src/elephant.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "dist/",
 
@@ -86,13 +89,26 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       publicPath: "",
+      filename: "hello-world.html",
+      chunks: ["hello-world"],
       title: "Webpack Sample",
       description: "Some Description",
-      filename: "index.html",
+      template: "src/page-template.hbs",
       // meta: {
       //   description: "Some Description",
       // },
-      template: "src/index.hbs",
+    }),
+    new HtmlWebpackPlugin({
+      publicPath: "",
+      title: "Webpack Sample",
+      description: "Some Description",
+      filename: "elephant.html",
+      chunks: ['elephant'],
+      template: "src/page-template.hbs",
+      // meta: {
+      //   description: "Some Description",
+      // },
+      
     }),
   ],
 };
